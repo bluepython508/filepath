0764f85cf0e8f7d203884be37579539cae24f8e0
bluepython508 <bluepython508@gmail.com>
2018-06-02 12:03:54 +0100

    Fixing upload.sh

--- upload.sh
+++ upload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 path=${1:-'.'}
-cd $path
-rm -rf $path/dist
-echo "Path: $path"
+cd ${path}
+rm -rf ${path}/dist
+echo "Path: ${path}"
 version=$(python package_version.py)
-read -p "Version (currently $version): " version
-echo "version='$version'" > $path/package_version.py
-echo 'print(version)' >> $path/package_version.py
-python $path/setup.py sdist bdist_wheel
-twine upload $path/dist/*
+read -p "Version (currently ${version}): " version
+echo "version='${version}'" > ${path}/package_version.py
+echo 'print(version)' >> ${path}/package_version.py
+python ${path}/setup.py sdist bdist_wheel
+twine upload ${path}/dist/*
 rm -rf dist
