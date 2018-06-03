84f69390d5e547f8ab292f1ad5ebb069cc3f51ec
bluepython508 <bluepython508@gmail.com>
2018-06-03 11:41:35 +0100

    Begun documentation

--- ostools/dirpath.py
+++ ostools/dirpath.py
@@ -1,14 +1,28 @@
-from . import FilePath
 import os
+
+from . import FilePath
 
 
 class Path:
-    def __init__(self, envvar):
+    """
+    This is a class for working with PATHs, like PATH or MANPATH.
+
+    It has the interface of a list. It loads the initial set from an environment variable,
+    and converts each element into a :class:`~ostools.FilePath`, with ``exist_required`` set to false.
+    """
+    def __init__(self, envvar='PATH'):
+        """
+        Args:
+            envvar (string): The environment variable to load the paths from.
+        """
         self.envvar = envvar
         self.items = []
         self.load()
 
     def load(self):
+        """
+        Loads the
+        """
         items = os.environ.get(self.envvar, '').split(':')
         for item in items:
             self.items.append(FilePath(item, exist_required=False))
@@ -19,6 +33,9 @@ class Path:
     def __contains__(self, item):
         return item in self.items
 
+    def __len__(self):
+        return len(self.items)
+
     def __getitem__(self, item):
         return self.items[item]
 
@@ -27,3 +44,13 @@ class Path:
 
     def __getattr__(self, item):
         return getattr(self.items, item)
+
+    def append(self, item):
+        self.items.append(FilePath(item, exist_required=False))
+
+    def extend(self, items):
+        for item in items:
+            self.append(item)
+
+    def insert(self, index, item):
+        self.items.insert(index, FilePath(item, exist_required=False))
